421ba5e0ede55a1d62483e2bba4fed9fb7d74b47
Anthony Salemo <ajssalemo@gmail.com>
2021-03-28 19:14:51 -0400

    adding a second route and a HTTP 404 catch all route handler

--- app.py
+++ app.py
@@ -25,7 +25,21 @@ def all_restaurants():
             all_restaurants_arr.append(restaurants)
         return jsonify(all_restaurants_arr)
     except Exception as e: 
-        print("An error has occurred while trying to retrieve results: ", e)
-        return jsonify({"error": "An error has occurred while trying to retrieve results"})
+        print(f"An error has occurred while trying to retrieve results: {e}")
+        return jsonify({"error": f"An error has occurred while trying to retrieve results: {e}"})
+
+
+# Find a restaurant by name
+@app.route('/restaurant/<name>')
+def find_restaurant(name):
+    print(name)
+    return jsonify({"message": f"Searching for {name}"})
+
+
+# HTTP 404 not found catch all route
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return jsonify({"error": "HTTP 404 - Page Not Found"}), 404
     
 
