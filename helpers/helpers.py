dceb04c862490e5504513ae2893e2215ad43a476
Anthony Salemo <ajssalemo@gmail.com>
2021-03-30 15:16:47 -0400

    removed a useless string concat in a jsonified response

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -7,7 +7,7 @@ def loop_through_response(arr, query):
 
     # If there are no results returned by the search then display an informational message
     if (len(arr) < 1):
-        return jsonify({"result": f"No restaurants found."})
+        return jsonify({"result": "No restaurants found."})
 
 
     
