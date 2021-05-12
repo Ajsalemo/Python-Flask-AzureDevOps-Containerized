638c700afa1b4425fd38d899342d6322decd6c24
Anthony Salemo <ajssalemo@gmail.com>
2021-05-12 19:45:05 -0400

    updated connection pool settings

--- database/database.py
+++ database/database.py
@@ -14,5 +14,7 @@ MONGO_ATLAST_DATABASE = os.environ.get("MONGO_ATLAST_DATABASE")
 
 # Connect to a Mongo Atlas cluster with this connection string
 CONN_STR=f"mongodb+srv://{MONGO_ATLAS_USERNAME}:{MONGO_ATLAS_PASSWORD}@{MONGO_ATLAS_CLUSTER}/{MONGO_ATLAST_DATABASE}?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONN_STR)
+# Set the max connection pool size to 128
+# Set the max idle time for connections in the pool to 30 seconds
+client = pymongo.MongoClient(CONN_STR, maxPoolSize=128, maxIdleTimeMS=30000)
 db = client.get_database(MONGO_ATLAST_DATABASE)
